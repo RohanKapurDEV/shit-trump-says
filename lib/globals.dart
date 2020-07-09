a80312e64d101b482ca35b9d6e9a05e7322cebe1
Rohan Kapur <rohankapurdev@outlook.com>
2020-07-09 19:13:41 +0800

    Adding more

---
+++ lib/globals.dart
@@ -0,0 +1,32 @@
+/// The colorSchemeArray contains arrays with 2 items each. The item at index 0 is the background
+/// color while the item at index 1 is the font color. These color schemes were pulled from designs
+/// I found all over the internet.
+List colorSchemeArray = [
+  [0xff415FF5, 0xffF2A88B],  // Purplish-blue, some kind of beige or something idk 
+  [0xffe52165, 0xff0d1137],  // Pink, Navy blue
+  [0xff12a4d9, 0xff322e2f],  // Light blue, Dark grey
+  [0xffF2A88B, 0xff415FF5],  // The first scheme, but in reverse
+  [0xfff3ca20, 0xff000000],  // Yellow, Black
+  [0xff1e3d59, 0xffff6e40],  // Navy, Orange
+  [0xffecc19c, 0xff000000],  // Tan, Black
+];
+
+List quotesArray = [
+  "\"I will build a great, great wall on our southern border, and I will have Mexico pay for that wall. Mark my words.\"",
+  "\"Mitt Romney had his chance to beat a failed president but he choked like a dog. Now he calls me racist-but I am least racist person there is\"",
+  "\"I don't think Ivanka would do that inside the magazine. Although she does have a very nice figure. I've said that if Ivanka weren't my daughter, perhaps I would be dating her.\"",
+  "\"You know, it doesn't really matter what they write as long as you've got a young and beautiful piece of ass.\"",
+  "\"You could see there was blood coming out of her eyes. Blood coming out of her... wherever.\"",
+  "\"I think apologizing's a great thing, but you have to be wrong. I will absolutely apologize, sometime in the hopefully distant future, if I'm ever wrong.\"",
+  "\"I mean, we could say politically correct that look doesn’t matter, but the look obviously matters ... Like you wouldn’t have your job if you weren’t beautiful.\"",
+  "\"I have a great relationship with African Americans, as you possibly have heard. I just have great respect for them. And they like me. I like them.\"",
+  "\"The U.S. cannot allow EBOLA-infected people back. People that go to far away places to help out are great - but must suffer the consequences!\"",
+  "\"It's really cold outside, they are calling it a major freeze, weeks ahead of normal. Man, we could use a big fat dose of global warming!\"",
+  "\"The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.\"",
+  "\"Well, if I ever ran for office, I'd do better as a Democrat than as a Republican - and that's not because I'd be more liberal, because I'm conservative.\"",
+  "\"To be blunt, people would vote for me. They just would. Why? Maybe because I'm so good looking.\"",
+  "\"If Hillary Clinton can't satisfy her husband what makes her think she can satisfy America?\"",
+  "\"If you look at Saddam Hussein, he killed terrorists. I'm not saying he was an angel, but this guy killed terrorists.\"",
+  "\"I never attacked him on his looks, and believe me, there's plenty of subject matter right there.\"",
+  "\"@arianahuff is unattractive both inside and out. I fully understand why her former husband left her for a man - he made a good decision.\"",
+];
