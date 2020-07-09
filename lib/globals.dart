049b4fda88b675cbe21c7abb388c17fbc77fe3d5
Rohan Kapur <rohankapurdev@outlook.com>
2020-07-09 21:28:07 +0800

    ...

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,17 +1,22 @@
 /// The colorSchemeArray contains arrays with 2 items each. The item at index 0 is the background
 /// color while the item at index 1 is the font color. These color schemes were pulled from designs
 /// I found all over the internet.
-List colorSchemeArray = [
+List<List> colorSchemeList = [
   [0xff415FF5, 0xffF2A88B],  // Purplish-blue, some kind of beige or something idk 
   [0xffe52165, 0xff0d1137],  // Pink, Navy blue
   [0xff12a4d9, 0xff322e2f],  // Light blue, Dark grey
-  [0xffF2A88B, 0xff415FF5],  // The first scheme, but in reverse
   [0xfff3ca20, 0xff000000],  // Yellow, Black
   [0xff1e3d59, 0xffff6e40],  // Navy, Orange
   [0xffecc19c, 0xff000000],  // Tan, Black
+  [0xffC3A9FF, 0xff000000],  // Light purple, Black
+  [0xff2026B2, 0xffffffff],  // Purple blue, White
+  [0xff5CDB94, 0xff05396B],  // Forest green, blue
+  [0xffFF4531, 0xff000000],  // Orange, black
+  [0xff4201FF, 0xffffffff],  // Purple blue, space grey
+  [0xff1A1A1C, 0xffDF0752],  // Some dark shit idk, some pink shit
 ];
 
-List quotesArray = [
+List quotesList = [
   "\"I will build a great, great wall on our southern border, and I will have Mexico pay for that wall. Mark my words.\"",
   "\"Mitt Romney had his chance to beat a failed president but he choked like a dog. Now he calls me racist-but I am least racist person there is\"",
   "\"I don't think Ivanka would do that inside the magazine. Although she does have a very nice figure. I've said that if Ivanka weren't my daughter, perhaps I would be dating her.\"",
@@ -29,4 +34,35 @@ List quotesArray = [
   "\"If you look at Saddam Hussein, he killed terrorists. I'm not saying he was an angel, but this guy killed terrorists.\"",
   "\"I never attacked him on his looks, and believe me, there's plenty of subject matter right there.\"",
   "\"@arianahuff is unattractive both inside and out. I fully understand why her former husband left her for a man - he made a good decision.\"",
+  "\"I look very much forward to showing my financials, because they are huge.\"",
+  "\"The concept of shaking hands is absolutely terrible, and statistically I've been proven right.\"",
+  "\"Sorry losers and haters, but my IQ is one of the highest - and you all know it! Please don't feel so stupid or insecure, it's not your fault.\"",
+  "\"I think Viagra is wonderful if you need it, if you have medical issues, if you've had surgery. I've just never needed it. Frankly, I wouldn't mind if there were an anti-Viagra, something with the opposite effect. I'm not bragging. I'm just lucky. I don't need it.\"",
+  "\"Certain guys tell me they want women of substance, not beautiful models. It just means they can't get beautiful models.\"",
+  "\"I would never buy Ivana any decent jewels or pictures. Why give her negotiable assets?\"",
+  "\"I have never seen a thin person drinking Diet Coke.\"",
+  "\"Show me someone with no ego and I'll show you a big loser.\"",
+  "\"I'm intelligent. Some people would say I'm very, very, very intelligent.\"",
+  "\"My fingers are long and beautiful, as, it has been well documented, are various other parts of my body.\"",
+  "\"I could stand in the middle of 5th Avenue and shoot somebody and I wouldn't lose voters.\"",
+  "\"Why are we having all these people from shithole countries coming here?\"",
+  "\"I loved my previous life. I had so many things going. This is more work than in my previous life. I thought it would be easier.\"",
+  "\"Look at my African-American!\"",
+  "\"I won’t do anything to take care of them. I’ll supply funds and she’ll take care of the kids. It’s not like I’m gonna be walking the kids down Central Park.\"",
+  "\"Why would Kim Jong-un insult me by calling me ‘old,’ when I would NEVER call him ‘short and fat?’\"",
+  "\"I would bet if you took a poll in the FBI I would win that poll by more than anybody’s won a poll.\"",
+  "\"Nobody has better respect for intelligence than Donald Trump.\"",
+  "\"Nobody knew health care could be so complicated.\"",
+  "\"I’m the least racist person you have ever interviewed.\"",
+  "\"Despite the constant negative press covfefe\"",
+  "\"What you’re seeing and what you’re reading is not what’s happening,\"",
+  "\"The beauty of me is that I’m very rich.\"",
+  "\"People love me. And you know what? I've been very successful. Everybody loves me.\"",
+  "\"A person who is very flat-chested is very hard to be a 10.\"",
+  "\"An 'extremely credible source' has called my office and told me that Barack Obama's birth certificate is a fraud.\"",
+  "\"The worst thing a man can do is go bald. Never let yourself go bald.\"",
+  "\"Is there a way we can do something like that by injection inside or almost a cleaning? It sounds interesting to me, so we'll see. But the whole concept of the light, the way it kills it in one minute. That's pretty powerful.\"",
+  "\"In another sense, I tested positively toward negative, right? So no. I tested perfectly this morning, meaning I tested negative. But that's a way of saying it. Positively toward the negative.\"",
+  "\"I don't take responsibility at all.\"",
+  "\"\"",
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:trumpApp/globals.dart';
+import 'dart:math';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -39,19 +42,162 @@ class _MainScreenState extends State<MainScreen> {
   @override
   void initState() {
     super.initState();
+    refreshQuoteAndColors();
   }
 
-  selectRandomColorSchemeArray() {
-    
+  selectRandomColorSchemeList() {
+    Random rnd = new Random();
+
+    final List subList = colorSchemeList[rnd.nextInt(colorSchemeList.length)];
+    setState(() {
+      this.backgroundColor = Color(subList[0]);
+      this.textColor = Color(subList[1]);
+
+      this.fabIconColor = Color(subList[0]);
+      this.fabBackgroundColor = Color(subList[1]);
+    });
   }
 
   selectRandomQuote() {
+    Random rnd = new Random();
 
+    final String freshQuote = quotesList[rnd.nextInt(quotesList.length)];
+    setState(() {
+      this.currentQuote = freshQuote;
+    });
+  }
+
+  refreshQuoteAndColors() {
+    setState(() {
+      selectRandomColorSchemeList();
+      selectRandomQuote();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: textColor),
+        backgroundColor: backgroundColor,
+        title: Text('Sh*t Donald Trump Says',
+            style: GoogleFonts.ubuntu(color: textColor)),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          refreshQuoteAndColors();
+        },
+        backgroundColor: fabBackgroundColor,
+        child: Icon(Icons.refresh, color: fabIconColor),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: backgroundColor,
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  'Information',
+                  style: GoogleFonts.ubuntu(color: textColor),
+                  textScaleFactor: 1.2,
+                ),
+                leading: Icon(Icons.info_outline, color: textColor),
+                onTap: () {
+                  Navigator.of(context, rootNavigator: true).push(
+                      new CupertinoPageRoute(
+                          builder: (_) => InformationView()));
+                },
+              ),
+              AboutListTile(
+                child: Text(
+                  'Licenses',
+                  style: GoogleFonts.ubuntu(color: textColor),
+                  textScaleFactor: 1.2,
+                ),
+                applicationVersion: '1.0.0',
+                icon: Icon(Icons.pages, color: textColor),
+              ),
+              SizedBox(height: 16),
+              ListTile(
+                title: Text(
+                  'Made by: Rohan Kapur\n\nTwitter: @RohanXKapur',
+                  style: GoogleFonts.ubuntu(color: textColor),
+                  textScaleFactor: 1.2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        color: backgroundColor,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            child: Text(
+              currentQuote,
+              style: GoogleFonts.merriweather(color: textColor, fontSize: 30),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class InformationView extends StatelessWidget {
+  const InformationView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Information',
+          style: GoogleFonts.ubuntu(),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              "I've tried to ensure the accuracy of the quotes to the best of my abilities but cannot and will not provide any guaruntees that everything is 100% accurate.",
+              textScaleFactor: 1.2,
+            ),
+            SizedBox(height: 16),
+            Text(
+              "If you believe any of the information is incorrect, feel free to contact me via Twitter and I'll be more than happy to make the necessary changes.",
+              textScaleFactor: 1.2,
+            ),
+            SizedBox(height: 16),
+            Text(
+              "Hopefully this goes without saying but just to be clear, this app was made for jokes and a laugh.",
+              textScaleFactor: 1.2,
+            ),
+            SizedBox(height: 16),
+            ListTile(
+              title: Text(
+                'Contact me on Twitter',
+                style: GoogleFonts.ubuntu(color: Colors.blue),
+              ),
+              onTap: () {
+                _launchTwitter();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+_launchTwitter() async {
+  const url = 'https://twitter.com/RohanXKapur';
+  if (await canLaunch(url)) {
+    await launch(url);
   }
 }
